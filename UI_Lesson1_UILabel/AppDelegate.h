ff66ac290ac0b9f8b762eced47a082f299942897
大龙 <dalong@dalongdeMacBook-Pro.local>
2015-10-12 16:09:28 +0800

    Initial Commit

---
+++ UI_Lesson1_UILabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI_Lesson1_UILabel
+//
+//  Created by 大龙 on 15/10/12.
+//  Copyright (c) 2015年 大龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI_Lesson1_UILabelTests/UI_Lesson1_UILabelTests.m
@@ -0,0 +1,40 @@
+//
+//  UI_Lesson1_UILabelTests.m
+//  UI_Lesson1_UILabelTests
+//
+//  Created by 大龙 on 15/10/12.
+//  Copyright (c) 2015年 大龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UI_Lesson1_UILabelTests : XCTestCase
+
+@end
+
+@implementation UI_Lesson1_UILabelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
